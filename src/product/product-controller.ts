0ea221257dfe1e65faf35af2b154fd3777a2083c
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2025-01-24 17:39:06 +0530

    Get product list

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -5,11 +5,12 @@ import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
 import { Logger } from "winston";
 import { ProductService } from "./product-service";
-import { Product } from "./product-types";
+import { Filter, Product } from "./product-types";
 import { FileStorage } from "../common/types/storage";
 import { v4 as uuidv4 } from "uuid";
 import { UploadedFile } from "express-fileupload";
 import { AuthRequest } from "../common/types";
+import mongoose from "mongoose";
 
 export class ProductController {
     private logger: Logger;
@@ -127,4 +128,28 @@ export class ProductController {
         await this.productService.updateProduct(productId, product);
         res.json({ id: productId });
     }
+
+    index = async (req: Request, res: Response) => {
+        //Example: /api/resource?q=search-term&tenantId=123&categoryId=456&isPublish=true
+        const { q, tenantId, categoryId, isPublish } = req.query;
+        const filters: Filter = {};
+
+        if (isPublish == "true") {
+            filters.isPublish = true;
+        }
+        if (tenantId) filters.tenantId = tenantId as string;
+        if (
+            categoryId &&
+            mongoose.Types.ObjectId.isValid(categoryId as string)
+        ) {
+            filters.categoryId = new mongoose.Types.ObjectId(
+                categoryId as string,
+            );
+        }
+        const products = await this.productService.getProducts(
+            q as string,
+            filters,
+        );
+        res.json(products);
+    };
 }
--- src/product/product-router.ts
+++ src/product/product-router.ts
@@ -53,4 +53,6 @@ router.put(
     asyncWrapper(productController.update),
 );
 
+router.get("/", asyncWrapper(productController.index));
+
 export default router;
